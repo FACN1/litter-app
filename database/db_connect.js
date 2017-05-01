d69cf489e5a022ba9a4a14d985962df743b38556
Jack Davies <jackwldavies@icloud.com>
2017-05-01 15:13:17 +0300

    spacing

--- database/db_connect.js
+++ database/db_connect.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const url = require('url');
 require('env2')('./config.env');
 
-if(!process.env.DATABASE_URL) throw new Error('Enviroment variable DB_URL must be set');
+if (!process.env.DATABASE_URL) throw new Error('Enviroment variable DB_URL must be set');
 
 const params = url.parse(process.env.DATABASE_URL);
 const [username, password] = params.auth.split(':');
