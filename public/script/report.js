441c37fea39ca4851698d24c6480426106740ed4
Edgar Dewsbery <edewsbery@gmail.com>
2017-05-01 19:01:29 +0300

    #34 add geolocator request to get-location button click handler

--- public/script/report.js
+++ public/script/report.js
@@ -1,5 +1,18 @@
 var locationButton = document.getElementById('getLocationButton');
 
-locationButton.addEventListener(function(e){
-  
-})
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    console.log(
+    "Latitude: " + position.coords.latitude +
+    "Longitude: " + position.coords.longitude
+  )
+}
+
+locationButton.addEventListener('click', getLocation)
