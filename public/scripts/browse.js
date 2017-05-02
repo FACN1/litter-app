9df4661d60cd1e31d88345c65e70e19de8aa5194
JWLD <jackwldavies@icloud.com>
2017-05-02 16:50:40 +0300

    #24 configure map to default to user location

--- public/scripts/browse.js
+++ public/scripts/browse.js
@@ -1,19 +1,26 @@
 (function() {
-  // set up map config
-  var lat = 50;
-  var lon = 50;
-
+  // set up map config - get mapbox token and location
   var mapBoxToken = document.getElementById('myMap').dataset.token;
 
+  // if no geolocation, set default location to London, UK
+  if (!navigator.geolocation) {
+    setUpMap(51.5285582, -0.2417011);
+  } else {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setUpMap(position.coords.latitude, position.coords.longitude);
+    });
+  }
+
   // instantiate map
-  var myMap = L.map('myMap').setView([lat, lon], 13);
+  function setUpMap(lat, lon) {
+    var myMap = L.map('myMap').setView([lat, lon], 13);
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-  {
-    attribution: null,
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: mapBoxToken
-  }).addTo(myMap);
-
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+    {
+      attribution: null,
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: mapBoxToken
+    }).addTo(myMap);
+  }
 })();
