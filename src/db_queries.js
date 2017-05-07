9a5cbbac4a5939beee8f8d4a1847064c8d8cef4c
JWLD <jackwldavies@icloud.com>
2017-05-07 12:45:24 +0300

    create DB query to return post data

--- src/db_queries.js
+++ src/db_queries.js
@@ -8,4 +8,13 @@ dbQueries.getMarkers = (connPool, callback) => {
   );
 };
 
+// get post data from DB
+dbQueries.getPostData = (connPool, id, callback) => {
+  connPool.query(
+    'SELECT * FROM posts WHERE id = $1',
+    [id],
+    callback
+  );
+};
+
 module.exports = dbQueries;
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,7 +1,18 @@
+const connPool = require('../../database/db_connect.js');
+const dbQueries = require('../db_queries.js');
+
 module.exports = {
   method: 'GET',
   path: '/posts',
   handler: (request, reply) => {
+    // request post data from DB using the url query parameter
+    dbQueries.getPostData(connPool, request.query.id, (err, res) => {
+      if (err) return console.log(err);
+
+      const postData = res.rows[0];
+      return console.log(postData);
+    });
+
     const context = {
       title: 'Posts Page'
     };
